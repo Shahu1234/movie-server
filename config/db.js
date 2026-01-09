34d80288bbc1d7a4976df95aa3a04a051624ca1d
Shahu1234 <shahajadi135@gmail.com>
2026-01-09 23:36:08 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,14 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import {connectDB} from './config/db.js'
-import cookieParser from 'cookie-parser'
-import userRoutes from './routers/user.router.js'
-const app=express()
-dotenv.config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+dotenv.config();
 
-app.use('/api/user', userRoutes);
-
-const port=process.env.PORT||8000
-connectDB()
-app.listen(port,()=>{
-    console.log(`server is running at http://localhost:${port}`)
-})
-app.get('/',(req,res)=>{
-    res.send("Welcome to The movie server home page")
-})
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
